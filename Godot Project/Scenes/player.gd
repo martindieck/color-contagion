0d1e5a76d976de71e7ac877b7f8bfecb02b8a0a2
martindieck <krutterlax@gmail.com>
2024-01-15 15:20:40 -0600

    Added Animation Playing

--- Godot Project/Scenes/player.gd
+++ Godot Project/Scenes/player.gd
@@ -1,6 +1,15 @@
 extends CharacterBody2D
 
+@onready var sprite_2d = $Sprite2D
+
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 600
 	move_and_slide()
+	
+	if direction != Vector2(0,0):
+		sprite_2d.play("movement")
+	else:
+		sprite_2d.play("idle")
+	
+	
