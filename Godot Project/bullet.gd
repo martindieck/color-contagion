f7ba6a85f3a19c6a5ab6e0a509f9b5636ebe3992
martindieck <krutterlax@gmail.com>
2024-01-16 11:02:43 -0600

    First Glimpses of Hope in Painting

--- Godot Project/bullet.gd
+++ Godot Project/bullet.gd
@@ -4,6 +4,8 @@ var travelled_distance = 0
 const SPEED = 1000
 const RANGE = 1200
 
+@onready var tilemap = get_node("/root/Game/Terrain")
+
 func _physics_process(delta):
 	var direction = Vector2.RIGHT.rotated(rotation)
 	position += direction * SPEED * delta
@@ -11,3 +13,6 @@ func _physics_process(delta):
 	travelled_distance += SPEED * delta
 	if travelled_distance > RANGE:
 		queue_free()
+
+func _on_body_entered(body):
+	tilemap.change_tileset(position)
--- Godot Project/terrain.gd
+++ Godot Project/terrain.gd
@@ -1,17 +1,48 @@
 extends TileMap
 
-var tile_size = 16 
+var tile_size = 16
+var tile_choice = FastNoiseLite.new()
 
-var width = get_viewport_rect().size.x/(tile_size - 1)
-var height = get_viewport_rect().size.y/(tile_size - 1)
+@onready var width = get_viewport_rect().size.x/(tile_size - 1)
+@onready var height = get_viewport_rect().size.y/(tile_size - 1)
 
 @onready var player = get_parent().get_child(1)
+
+var tile_dict = {}
+
+func change_tileset(bullet_position):
+	var tile_pos = local_to_map(bullet_position)
+	var x_coord = tile_pos.x
+	var y_coord = tile_pos.y
+	var dict_key = str(Vector2(x_coord,y_coord))
+	tile_dict[dict_key] = true
 
 func _process(delta):
 	generate_chunk(player.position)
 	
-func generate_chunk(position):
-	var tile_pos = local_to_map(position)
+func generate_chunk(player_position):
+	var tile_pos = local_to_map(player_position)
 	for x in range(width):
 		for y in range(height):
-			set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y - height/2 + y), 0, Vector2i(4,0))
+			var x_coord = tile_pos.x-width/2 + x
+			var y_coord = tile_pos.y - height/2 + y
+			var dict_key = str(Vector2(x_coord,y_coord))
+			if dict_key in tile_dict:
+				if tile_dict[dict_key] == true:
+					var choice = tile_choice.get_noise_2d(x_coord, y_coord)*10
+					set_cell(0, Vector2i(x_coord, y_coord), 0, Vector2i(round((choice+10)/5), 1))
+			else:
+				var choice = tile_choice.get_noise_2d(x_coord, y_coord)*10
+				set_cell(0, Vector2i(x_coord, y_coord), 0, Vector2i(round((choice+10)/5), 0))
+				tile_dict[dict_key] = false
+
+
+	#var choice = tile_choice.get_noise_2d(tile_pos.x-width/2, tile_pos.y - height/2)*10
+	#print(tile_pos)
+	#set_cell(0, Vector2i(tile_pos.x, tile_pos.y), 0, Vector2i(round((choice+10)/5),1))
+	#for x in range(width):
+		#for y in range(height):
+			#var choice = tile_choice.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y - height/2 + y)*10
+			#set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y - height/2 + y), 0, Vector2i(round((choice+10)/5),1))
+	
+
