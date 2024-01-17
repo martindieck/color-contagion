d0b5619f203c637f954d1e57850be87c14e00516
martindieck <krutterlax@gmail.com>
2024-01-16 20:49:47 -0600

    Added First Round

--- Godot Project/Scenes/game.gd
+++ Godot Project/Scenes/game.gd
@@ -3,6 +3,7 @@ extends Node2D
 #Rule of Thumb: 3500 per 10 sec for regular gun
 
 @onready var tile_counter = get_node("/root/Game/Player/ColorRect/TileCounter")
+var objective = 50000
 
 func _ready():
 	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -10,5 +11,8 @@ func _ready():
 	add_child(target)
 
 func _physics_process(delta):
-	tile_counter.text = str(Global.tile_count)
+	tile_counter.text = str(Global.tile_count) + " / " + str(objective)
 	#tile_counter.text = str(Engine.get_frames_per_second())
+
+func _on_round_timer_timeout():
+	print(Global.tile_count)
