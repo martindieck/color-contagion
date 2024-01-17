4355cb359664396517a29784e0ef6516b6fe61d1
martindieck <krutterlax@gmail.com>
2024-01-16 20:59:26 -0600

    Added Round quota and time system

--- Godot Project/Scenes/game.gd
+++ Godot Project/Scenes/game.gd
@@ -3,16 +3,25 @@ extends Node2D
 #Rule of Thumb: 3500 per 10 sec for regular gun
 
 @onready var tile_counter = get_node("/root/Game/Player/ColorRect/TileCounter")
-var objective = 50000
+
+var quotas = [50000, 120000, 220000, 400000]
+var round_times = [180, 180, 180, 180]
+var rounds = {}
+var current_round = 1
 
 func _ready():
 	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	var target = preload("res://Scenes/target.tscn").instantiate()
 	add_child(target)
+	create_rounds_dict(quotas, round_times)
 
 func _physics_process(delta):
-	tile_counter.text = str(Global.tile_count) + " / " + str(objective)
+	tile_counter.text = str(Global.tile_count) + " / " + str(rounds[current_round]["quota"])
 	#tile_counter.text = str(Engine.get_frames_per_second())
 
 func _on_round_timer_timeout():
 	print(Global.tile_count)
+
+func create_rounds_dict(quotas, round_times):
+	for round in range(quotas.size()):
+		rounds[round + 1] = {"quota": quotas[round], "time": round_times[round]}
