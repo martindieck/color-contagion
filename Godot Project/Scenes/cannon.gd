8702fd43fad2b6b9a11a44c12c9efb69e818fb06
martindieck <krutterlax@gmail.com>
2024-01-19 20:37:06 -0600

    Added Cannon and Missile Scenes

---
+++ Godot Project/Scenes/cannon.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+func _physics_process(delta):
+	var mouse_position = get_global_mouse_position()
+	var direction = mouse_position - global_position
+	var angle = atan2(direction.y, direction.x)
+	rotation = angle
+	
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+	
+func shoot():
+	const BULLET = preload("res://Scenes/bullet.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child(new_bullet)
+
+func _on_shoot_timer_timeout():
+	if Input.is_action_pressed("shoot"):
+		shoot()
