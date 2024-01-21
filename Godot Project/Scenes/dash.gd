657756283905ad6ef45b02374ba5175ae45c759b
martindieck <krutterlax@gmail.com>
2024-01-21 14:21:24 -0600

    Added Dash Item

---
+++ Godot Project/Scenes/dash.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var player = get_node("/root/Game/Player")
+@onready var cool_down = $CoolDown
+@onready var dash_timer = $DashTimer
+
+var can_dash = true
+
+func _process(delta):
+	if Input.is_action_just_pressed("movement") and can_dash:
+		can_dash = false
+		player.speed = 2000
+		cool_down.start()
+		dash_timer.start()
+
+func _on_cool_down_timeout():
+	can_dash = true
+
+func _on_dash_timer_timeout():
+	player.speed = 600
--- Godot Project/Scenes/player.gd
+++ Godot Project/Scenes/player.gd
@@ -2,11 +2,16 @@ extends CharacterBody2D
 
 @onready var sprite_2d = $Sprite2D
 @onready var weapon_holder = $WeaponHolder
+@onready var item_holder = $ItemHolder
+
+var can_dash = true
+var speed = 600
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * 600
+	velocity = direction * speed
 	move_and_slide()
+	
 	
 	if direction != Vector2(0,0):
 		sprite_2d.play("movement")
@@ -14,7 +19,7 @@ func _physics_process(delta):
 		sprite_2d.play("idle")
 	
 	if Input.is_action_just_pressed("change"):
-		change_weapon("minigun")
+		add_item("dash")
 		
 func change_weapon(weapon):
 	for child in weapon_holder.get_children():
@@ -32,3 +37,10 @@ func change_weapon(weapon):
 			const WEAPON = preload("res://Scenes/flamethrower.tscn")
 			var new_weapon = WEAPON.instantiate()
 			weapon_holder.add_child(new_weapon)
+
+func add_item(item):
+	match item:
+		"dash":
+			const ITEM = preload("res://Scenes/dash.tscn")
+			var new_item = ITEM.instantiate()
+			item_holder.add_child(new_item)
