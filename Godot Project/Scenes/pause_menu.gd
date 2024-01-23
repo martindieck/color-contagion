700bc89fdbb33cb46d24ab21dca2585a4ccbfbbf
martindieck <krutterlax@gmail.com>
2024-01-23 00:01:17 -0600

    Added Pause Menu
    And General UI Fixes and Tweaks

---
+++ Godot Project/Scenes/pause_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+var paused = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("pause") and not paused:
+		get_tree().paused = true
+		paused = true
+		show()
+	elif Input.is_action_just_pressed("pause") and paused:
+		get_tree().paused = false
+		paused = false
+		hide()
