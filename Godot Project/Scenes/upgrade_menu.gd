556aaed6cc8bb00ab058dd50a43f51cfc1d50b3b
martindieck <krutterlax@gmail.com>
2024-01-25 17:35:55 -0600

    Added Weapon and Item Tooltips

--- Godot Project/Scenes/upgrade_menu.gd
+++ Godot Project/Scenes/upgrade_menu.gd
@@ -5,11 +5,16 @@ var choice2 : String
 var choice3 : String
 var type : String
 var pre_text = "Choose your "
+var tooltip1
+var tooltip2
+var tooltip3
+var bigtext = "Press Shift to repel enemies around you. You're protected while it is active. (10 sec cooldown)"
 
 @onready var choice1_but = %Choice1
 @onready var choice2_but = %Choice2
 @onready var choice3_but = %Choice3
 @onready var upgrade_label = %UpgradeLabel
+@onready var tooltext = $MarginContainer/VBoxContainer/ToolText
 
 @onready var player = get_node("/root/Game/Player")
 
@@ -23,24 +28,30 @@ func upgrade():
 			upgrade_label.text = pre_text + type + ":"
 			choice1_but.text = "Minigun"
 			choice1_but.icon = load("res://Sprites/minigun.png")
+			tooltip1 = "Hold Left Click to start the wind up and fire rapidly."
 			choice1 = "minigun"
 			choice2_but.text = "Cannon"
 			choice2_but.icon = load("res://Sprites/Cannon.png")
+			tooltip2 = "Fire hard hitting missiles that convert a large area."
 			choice2 = "cannon"
 			choice3_but.text = "Flamethrower"
 			choice3_but.icon = load("res://Sprites/Flamethrower.png")
+			tooltip3 = "Hold Left Click to fire a rapid stream of flames."
 			choice3 = "flamethrower"
 		3:
 			type = "item"
 			upgrade_label.text = pre_text + type + ":"
 			choice1_but.text = "Dash"
 			choice1_but.icon = load("res://Sprites/Dash.png")
+			tooltip1 = "Press Shift to get a burst of speed. You're invincible while dashing. (10 sec cooldown)"
 			choice1 = "dash"
 			choice2_but.text = "Repulsion"
 			choice2_but.icon = load("res://Sprites/Repulsion.png")
+			tooltip2 = "Press Shift to repel enemies around you. You're protected while it is active. (10 sec cooldown)"
 			choice2 = "repulsion"
-			choice3_but.text = "Shield"
+			choice3_but.text = "Shield of Color"
 			choice3_but.icon = load("res://Sprites/Shield.png")
+			tooltip3 = "Prevents an enemy hit. Recharges in 20 seconds. Also leaves a trail of color while equipped."
 			choice3 = "shield"
 
 func _on_choice_1_pressed():
@@ -69,3 +80,27 @@ func _on_choice_3_pressed():
 		player.change_weapon(choice3)
 	elif type == "item":
 		player.add_item(choice3)
+
+func _on_choice_1_mouse_entered():
+	tooltext.text = "[center]%s[/center]" % tooltip1
+	tooltext.add_theme_color_override("default_color", Color(1, 1, 1, 1))
+	
+func _on_choice_1_mouse_exited():
+	tooltext.add_theme_color_override("default_color", Color(0.4, 0.4, 0.4, 1))
+	tooltext.text = bigtext
+	
+func _on_choice_2_mouse_entered():
+	tooltext.text = "[center]%s[/center]" % tooltip2
+	tooltext.add_theme_color_override("default_color", Color(1, 1, 1, 1))
+
+func _on_choice_2_mouse_exited():
+	tooltext.add_theme_color_override("default_color", Color(0.4, 0.4, 0.4, 1))
+	tooltext.text = bigtext
+
+func _on_choice_3_mouse_entered():
+	tooltext.text = "[center]%s[/center]" % tooltip3
+	tooltext.add_theme_color_override("default_color", Color(1, 1, 1, 1))
+
+func _on_choice_3_mouse_exited():
+	tooltext.text = bigtext
+	tooltext.add_theme_color_override("default_color", Color(0.4, 0.4, 0.4, 1))
