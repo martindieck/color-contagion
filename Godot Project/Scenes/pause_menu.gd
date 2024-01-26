f73f91d75cf626a24c37ded0866131afa1cb9f55
martindieck <krutterlax@gmail.com>
2024-01-26 16:25:21 -0600

    Added Pause Menu "Muffle" Sound
    Using Audio Buses

--- Godot Project/Scenes/pause_menu.gd
+++ Godot Project/Scenes/pause_menu.gd
@@ -1,20 +1,25 @@
 extends CanvasLayer
 
+@onready var music = get_node("/root/Game/MusicPlayer")
+
 func _process(delta):
 	if Input.is_action_just_pressed("pause") and not Global.paused and not Global.in_cutscene:
 		#If you press pause and it is not paused and you're not in cutscene
 		get_tree().paused = true
 		Global.paused = true
 		show()
+		music.set_bus("Low")
 	elif Input.is_action_just_pressed("pause") and Global.paused:
 		#Or if you pressed pause and it is already paused.
 		if Global.is_upgrading:
 			Global.paused = false
 			hide()
+			music.set_bus("Master")
 		else:
 			get_tree().paused = false
 			Global.paused = false
 			hide()
+			music.set_bus("Master")
 
 func _on_resume_pressed():
 	if Global.paused:
@@ -34,4 +39,5 @@ func _on_transition_screen_transitioned():
 	hide()
 	get_tree().paused = false
 	Global.paused = false
+	music.set_bus("Master")	
 	get_tree().change_scene_to_file(Global.next_scene)
